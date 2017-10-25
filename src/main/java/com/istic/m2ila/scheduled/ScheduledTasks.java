3fe6e2bb9d6f65315cc5e848264c812bfb6a5ea1
czbirka <christophe.zbirka@wanadoo.fr>
2017-10-25 22:13:09 +0200

    GET request to weather API

--- src/main/java/com/istic/m2ila/scheduled/ScheduledTasks.java
+++ src/main/java/com/istic/m2ila/scheduled/ScheduledTasks.java
@@ -1,9 +1,17 @@
 package com.istic.m2ila.scheduled;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonParser;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +21,12 @@ import com.istic.m2ila.model.User;
 import com.istic.m2ila.service.ActiviteDAO;
 import com.istic.m2ila.service.LieuDAO;
 import com.istic.m2ila.service.UserDAO;
+
+
+
+
+
+
 
 @Component
 public class ScheduledTasks {
@@ -33,6 +47,8 @@ public class ScheduledTasks {
     public void test() {
     	List<User> users = userDao.findAll();
     	List<Activite> activites = activiteDao.findAll();
+    	
+    	String donneesMeteo = "";
     	
     	System.out.println("USERS : " + users.size());
     	System.out.println("ACTIVITES : " + activites.size());
@@ -71,7 +87,18 @@ public class ScheduledTasks {
     		for (int j=0; j<lieuxUser.size(); j++) {
     			System.out.println(lieuxUser.get(j).getId()+" - "+lieuxUser.get(j).getNom());
     		}
+    		if (lieuxUser.size()>0) {
+    		double lat = lieuxUser.get(0).getLatitude();
+    		double lon = lieuxUser.get(0).getLongitude();
+    		try {
+    			donneesMeteo=sendGET();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    		}
     	}
+    	
     	
     	
     	
@@ -106,4 +133,44 @@ public class ScheduledTasks {
    // 	});
     }
 	
+    
+    private  String sendGET() throws IOException {
+    	 String GET_URL = "http://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&APPID=79ab92ee7aba089ef7c6dbb6c96c9a54&units=metric";
+    	 String USER_AGENT = "Mozilla/5.0";
+    	 
+		URL obj = new URL(GET_URL);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		int responseCode = con.getResponseCode();
+		
+		System.out.println("GET Response Code :: " + responseCode);
+		
+		if (responseCode == HttpURLConnection.HTTP_OK) { // success
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			//StringBuffer response = new StringBuffer();
+			String response ="";
+
+			while ((inputLine = in.readLine()) != null) {
+				//response.append(inputLine);
+				response += inputLine;
+			}
+			in.close();
+
+			System.out.println(response);
+			
+			return response;
+			
+			//JSONObject objet = new JSONObject();
+			
+			
+		} else {
+			System.out.println("GET request not worked");
+			return null;
+		}
+
+	}
+    
+
 }
