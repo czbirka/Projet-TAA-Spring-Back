fcd4208f456df87e622c726b87febd32129478fe
masoumiaa <aminesoumiaa@gmail.com>
2017-10-31 12:31:52 +0100

    [Backend][Mailer] : Add mailing Service

---
+++ src/main/java/com/istic/m2ila/scheduled/MailingService.java
@@ -0,0 +1,29 @@
+package com.istic.m2ila.scheduled;
+
+import java.io.InputStream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MailingService {
+   // @Autowired
+	private MailSender mailSender;
+
+	public void setMailSender(MailSender mailSender) {
+		this.mailSender = mailSender;
+	}
+
+	public void sendMail(String from, String to, String subject, String msg) {
+
+		SimpleMailMessage message = new SimpleMailMessage();
+
+		message.setFrom(from);
+		message.setTo(to);
+		message.setSubject(subject);
+		message.setText(msg);
+		mailSender.send(message);
+	}
+}
